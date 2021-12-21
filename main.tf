abcbc2e1e160d935bf8c63562a2776590d3dcd30
cliff1325 <cliff1325>
2021-12-21 18:48:53 -0500

    initial code

--- main.tf
+++ main.tf
@@ -1,43 +1,21 @@
-terrafrom {
-required_providers {
-azurerm = {
-source = "hasicorp/azurerm"
-version = "1.1.2"
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
 
-
-}
-}
-
-
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
-  
+  features {}
 }
 
-resource "azure_resource_group" "myrg" {
-    name = "clifford-resource-rg"
-    location = "Central US"
-  
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "CentralUS"
 }
 
-
-resource "azure_virtual_network" "myvnet" {
-    name = "clifford-vnet"
-    resource_group_name = azurerm_resource_group.myrg.name
-    location = azurerm_resource_group.myrg.location
-    address_space = ["10.0.0.0/16"]
-
-  
-}
-
-resource "azurerm_subnet" "mysubnet" {
-  
-  name = "clifford-subnet"
-    resource_group_name = azurerm_resource_group.myrg.name
-    virtual_network_name = azurerm_virtual_network.myvnet.name
-    address_prefix= ["10.0.0.0/24"]
-
-  
-
-}
